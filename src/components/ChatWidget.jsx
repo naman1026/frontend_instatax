f1c51a4a181b004e67f971e189ea43cfd147a0c0
kunal <deopakunal@gmail.com>
2025-03-12 15:19:04 +0530

    Add company icon in chat button

--- src/components/ChatWidget.jsx
+++ src/components/ChatWidget.jsx
@@ -1,11 +1,12 @@
-// src/components/ChatWidget.jsx
 import React from "react";
 import "./ChatWidget.css";
+import chatIcon from "../assets/smalllogo.png"; // Import your image
 
 const ChatWidget = () => {
   return (
     <div className="chat-widget">
       <button className="chat-btn">
+        <img src={chatIcon} alt="Chat" className="chat-icon" />
         <span>Chat with Us!</span>
       </button>
     </div>
