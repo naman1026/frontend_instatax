a93dbe8e0f98aa2e08e6aa2de0c90131d5e368df
kunal <deopakunal@gmail.com>
2025-04-05 14:05:55 +0530

    integrated API to submit quotation details

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>
